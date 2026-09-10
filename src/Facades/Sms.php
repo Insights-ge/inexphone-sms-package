a2a3a78e0893056c0559be1189023c134bfc65d1
PrayerPosition <nika.kapana23@gmail.com>
2026-09-10 15:13:35 +0400

    feat: add OTP methods to facade

--- src/Facades/Sms.php
+++ src/Facades/Sms.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static array<string, mixed> sendBulk(string $subject, string $message, array<int, string> $phoneNumbers, ?string $submitCallbackUrl = null, ?string $deliveryCallbackUrl = null)
  * @method static array<string, mixed> list(array<string, mixed> $params = [])
  * @method static array<string, mixed> find(string $uuid)
+ * @method static array<string, mixed> sendOtp(string $phone, string $subject, ?string $text = null, ?int $expiresIn = null, ?int $codeDigits = null)
+ * @method static array<string, mixed> verifyOtp(string $phone, string $code)
  */
 class Sms extends Facade
 {
